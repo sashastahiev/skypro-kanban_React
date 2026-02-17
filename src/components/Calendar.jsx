b5cf4b029c0a9faa17d5d62dfe191499d6edb5fe
sashastahiev <vebium@mail.ru>
2026-02-18 00:04:40 +0300

    commit separation into components

---
+++ src/components/Calendar.jsx
@@ -0,0 +1,56 @@
+function Calendar() {
+  return (
+    <div class="calendar__content">
+        <div class="calendar__days-names">
+        <div class="calendar__day-name">пн</div>
+        <div class="calendar__day-name">вт</div>
+        <div class="calendar__day-name">ср</div>
+        <div class="calendar__day-name">чт</div>
+        <div class="calendar__day-name">пт</div>
+        <div class="calendar__day-name -weekend-">сб</div>
+        <div class="calendar__day-name -weekend-">вс</div>
+        </div>
+        <div class="calendar__cells">
+        <div class="calendar__cell _other-month">28</div>
+        <div class="calendar__cell _other-month">29</div>
+        <div class="calendar__cell _other-month">30</div>
+        <div class="calendar__cell _cell-day">31</div>
+        <div class="calendar__cell _cell-day">1</div>
+        <div class="calendar__cell _cell-day _weekend">2</div>
+        <div class="calendar__cell _cell-day _weekend">3</div>
+        <div class="calendar__cell _cell-day">4</div>
+        <div class="calendar__cell _cell-day">5</div>
+        <div class="calendar__cell _cell-day ">6</div>
+        <div class="calendar__cell _cell-day">7</div>
+        <div class="calendar__cell _cell-day _current">8</div>
+        <div class="calendar__cell _cell-day _weekend">9</div>
+        <div class="calendar__cell _cell-day _weekend">10</div>
+        <div class="calendar__cell _cell-day">11</div>
+        <div class="calendar__cell _cell-day">12</div>
+        <div class="calendar__cell _cell-day">13</div>
+        <div class="calendar__cell _cell-day">14</div>
+        <div class="calendar__cell _cell-day">15</div>
+        <div class="calendar__cell _cell-day _weekend">16</div>
+        <div class="calendar__cell _cell-day _weekend">17</div>
+        <div class="calendar__cell _cell-day">18</div>
+        <div class="calendar__cell _cell-day">19</div>
+        <div class="calendar__cell _cell-day">20</div>
+        <div class="calendar__cell _cell-day">21</div>
+        <div class="calendar__cell _cell-day">22</div>
+        <div class="calendar__cell _cell-day _weekend">23</div>
+        <div class="calendar__cell _cell-day _weekend">24</div>
+        <div class="calendar__cell _cell-day">25</div>
+        <div class="calendar__cell _cell-day">26</div>
+        <div class="calendar__cell _cell-day">27</div>
+        <div class="calendar__cell _cell-day">28</div>
+        <div class="calendar__cell _cell-day">29</div>
+        <div class="calendar__cell _cell-day _weekend">30</div>
+        <div class="calendar__cell _other-month _weekend">
+            1
+        </div>
+        </div>
+    </div>
+  );
+}
+
+export default Calendar
