b6446396c31d308188f8e04da56541d231e99e02
sashastahiev <vebium@mail.ru>
2026-02-26 19:56:41 +0300

    commit style main file

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,51 @@
 import Column from "../components/Column";
+import styled from "styled-components";
+const Smain = styled.main`
+  width: 100%;
+  background-color: #eaeef6;
+`
+const Scontainer = styled.main`
+  max-width: 1260px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 30px;
+  @media screen and (max-width: 495px) {
+    width: 100%;
+    padding: 0 16px;
+  }
+`
+const SmainBlock = styled.main`
+  width: 100%;
+  margin: 0 auto;
+  padding: 25px 0 49px;
+  @media screen and (max-width: 1200px) {
+    width: 100%;
+    margin: 0 auto;
+    padding: 40px 0 64px;
+  }
+`
+const SmainContent = styled.main`
+  width: 100%;
+  display: flex;
+  @media screen and (max-width: 1200px) {
+    display: block;
+  }
+`
 function Main() {
   return (
-    <main className="main">
-      <div className="container">
-        <div className="main__block">
-          <div className="main__content">
+    <Smain>
+      <Scontainer>
+        <SmainBlock>
+          <SmainContent>
             <Column text="Без статуса"/>
             <Column text="Нужно сделать"/>
             <Column text="В работе"/>
             <Column text="Тестирование"/>
             <Column text="Готово"/>
-          </div>
-        </div>
-      </div>
-    </main>
+          </SmainContent>
+        </SmainBlock>
+      </Scontainer>
+    </Smain>
   );
 }
 
